113cfb951ee907f6a45a9e7d2163ceb2cfe669e9
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2018-11-29 11:19:33 -0600

    intial push

---
+++ Polygons/Polygons/Program.cs
@@ -0,0 +1,42 @@
+﻿using Polygons.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Square _square = new Square(100);
+            DisplayPolygon("Square", _square);
+
+            Console.ReadLine();
+        }
+
+
+        public static void DisplayPolygon(string polygonType, dynamic polygon)
+        {
+            try
+            {
+                Console.WriteLine("{0} Number of Sides: {1}", polygonType, polygon.NumberOfSides);
+                Console.WriteLine("{0} Side Length: {1}", polygonType, polygon.SideLength);
+                Console.WriteLine("{0} Perimeter: {1}", polygonType, polygon.GetPerimeter());
+                Console.WriteLine("{0} Area: {1}", polygonType, Math.Round(polygon.GetArea(), 2));
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception thrown while trying to process {0}:\n   {1}",
+                    polygonType, ex.GetType().Name);
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
